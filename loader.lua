91a0237b15e36757627a8a855a0accf62e3a2cdd
WisnuX67 <amanahfrbro@gmail.com>
2026-09-04 13:33:10 +0800

    update: tambah ViolenceDistrict ke loader

--- loader.lua
+++ loader.lua
@@ -1,15 +1,17 @@
 local games = {
-    -- [UniverseId atau PlaceId] = "url script"
-    -- SAE
+    -- Steal An Egg
     [10563114921]     = "https://raw.githubusercontent.com/WisnuX67/WisnuX/main/StealAnEgg.lua",
     [107778070777162] = "https://raw.githubusercontent.com/WisnuX67/WisnuX/main/StealAnEgg.lua",
+    -- Violence District
+    [6739698191]      = "https://raw.githubusercontent.com/WisnuX67/WisnuX/main/ViolenceDistrict.lua",
+    [93978595733734]  = "https://raw.githubusercontent.com/WisnuX67/WisnuX/main/ViolenceDistrict.lua",
     -- tambah game lain di sini
 }
 
 local scriptURL = games[game.GameId] or games[game.PlaceId]
 
 if scriptURL then
-    print("[WisnuX] Loading for:", game.GameId, "/", game.PlaceId)
+    print("[WisnuX] Loading for GameId=" .. tostring(game.GameId))
     loadstring(game:HttpGet(scriptURL))()
 else
     warn("[WisnuX] Game not supported. GameId=" .. tostring(game.GameId) .. " PlaceId=" .. tostring(game.PlaceId))
